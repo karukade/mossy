c54b56731c0ded9f308f8d9f7ec81a8c0cd95f20
kadeshiro <gaaadaaa16@gmail.com>
2021-03-16 16:29:01 +0900

    line middlewareを追加 signature validationのため

--- pages/api/bot.ts
+++ pages/api/bot.ts
@@ -1,9 +1,53 @@
-import type { NextApiRequest, NextApiResponse } from "next"
+import { SignatureValidationFailed } from "@line/bot-sdk"
+import { IncomingMessage, ServerResponse } from "http"
 import { webhookHandler } from "~/lib/bot/handlers"
-import lineClient, { LineEventObject } from "~/lib/bot/lineClient"
+import lineClient, { LineEventObject, middleWare } from "~/lib/bot/lineClient"
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+/**
+ * line middleware を使うのでbodyParserはfalse
+ */
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+const runMiddleware = <Req, Res>(
+  req: Req,
+  res: Res,
+  fn: (req: Req, res: Res, next: (result: any) => void) => void
+) => {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+      return resolve(result)
+    })
+  })
+}
+
+export default async (
+  req: IncomingMessage & { body: any },
+  res: ServerResponse
+) => {
   if (req.method !== "POST") {
+    res.end()
+    return
+  }
+
+  try {
+    await runMiddleware(req, res, middleWare)
+  } catch (e) {
+    if (!(e instanceof SignatureValidationFailed)) {
+      console.log("ERROR", e)
+      await lineClient.pushMessage("U97c820c6e12abbbbccfb8a862040396b", {
+        type: "text",
+        text: `エラーが発生しました。\n${JSON.stringify(e)}`,
+      })
+    } else {
+      console.log("signature error")
+    }
     res.end()
     return
   }
@@ -12,12 +56,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     await Promise.all(body.events.map((event) => webhookHandler(event)))
   } catch (e) {
+    // TODO: next.jsでエラーログとれるようにしたい
     console.log("ERROR", e)
     await lineClient.pushMessage("U97c820c6e12abbbbccfb8a862040396b", {
       type: "text",
       text: `エラーが発生しました。\n${JSON.stringify(e)}`,
     })
-    res.json(e)
   }
 
   res.end()
